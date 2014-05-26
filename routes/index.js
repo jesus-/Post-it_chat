079f085a21d8503e0c4737febfefdd915866f640
jesus- <chusma.ellagarto@gmail.com>
2014-05-26 12:44:11 +0800

    chaging redirect

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
+router.get('/', function(req, res) {
+    res.redirect('/chat');
+    // res.render('/chat', { title: 'post-it' });
 });
 //get the messages
 router.get('/chat', function(req, res) {
